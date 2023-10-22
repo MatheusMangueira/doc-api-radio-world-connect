464d335454eb9f1153175ad7b533c2821ab7c7fe
MatheusMangueira <matheusmangueira16@hotmail.com>
2023-10-22 15:00:05 -0300

    feat: create component ball

---
+++ src/app/components/BallYellow/BallYellow.tsx
@@ -0,0 +1,12 @@
+export const BallYellow = () => {
+  return (
+    <div
+      className="absolute rounded-full lg:w-[648px] lg:h-[648px]
+      md:w-[400px] md:h-[400px] w-[300px] h-[300px]
+      flex-shrink
+    bg-ball-yellow shadow-3xl z-[-1] lg:top-[-150px] lg:left-[100px]
+    md:top-[-90px] md:left-[50px] top-[0px] left-[0px]
+    "
+    />
+  );
+};
