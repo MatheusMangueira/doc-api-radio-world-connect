aaf61ee1834c039279ca5316559d6df9bcf5c34b
MatheusMangueira <matheusmangueira16@hotmail.com>
2023-10-23 17:02:05 -0300

    feat: new page (newStation)

---
+++ src/app/newStation/page.tsx
@@ -0,0 +1,9 @@
+import { NewStationsView } from "../views/newStationsView";
+
+export default function NewStation() {
+  return (
+    <main className="lg:px-10 p-2 max-w-[1920px] flex justify-center w-full">
+      <NewStationsView />
+    </main>
+  );
+}
