d23195ff94e50893fda66358b3c954300afd074d
MatheusMangueira <matheusmangueira16@hotmail.com>
2023-10-25 19:34:01 -0300

    fix: add message for radio

--- src/app/views/about/aboutText.ts
+++ src/app/views/about/aboutText.ts
@@ -3,12 +3,17 @@ export const about = [
     id: 1,
     name: 'Quem é a Radio World Connect?',
     description:
-      'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic'
+      'A Radio World Connect é uma iniciativa dedicada a centralizar e compartilhar uma vasta coleção de estações de rádio de diversas localidades e gêneros musicais. Através desta API, os apaixonados por rádio terão a capacidade de acessar, contribuir e atualizar uma variedade crescente de estações, proporcionando uma experiência dinâmica e envolvente para usuários em todo o mundo.'
   },
   {
     id: 2,
-    name: 'Quem é a Radio World Connect?',
+    name: 'Nossa Comunidade, Nossa Força',
     description:
-      'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic'
+      'A força da Radio World Connect reside na sua comunidade. Encorajamos fervorosamente a participação ativa de desenvolvedores, entusiastas de rádio e amantes da tecnologia para contribuírem com melhorias, novas estações e recursos adicionais. Juntos, moldamos e aprimoramos essa plataforma, tornando-a uma referência confiável para os amantes da rádio ao redor do planeta.'
+  },
+  {
+    id: 3,
+    name: 'Nossa Missão',
+    description: 'A API Radio World Connect é uma ferramenta versátil, pronta para ser incorporada em projetos pessoais e comerciais. Seja para criar aplicativos móveis personalizados, integrar em sistemas de entretenimento ou desenvolver soluções inovadoras para a indústria de transmissão. '
   }
 ];
