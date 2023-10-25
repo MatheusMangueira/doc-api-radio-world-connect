b3c620eccebe7a354780a1ca517e60633d5d0404
MatheusMangueira <matheusmangueira16@hotmail.com>
2023-10-25 19:33:43 -0300

    fix: grid template

--- src/app/views/about/About.tsx
+++ src/app/views/about/About.tsx
@@ -2,7 +2,7 @@ import { about } from './aboutText';
 
 export const About = () => {
   return (
-    <div className="border-t border-black border-opacity-20 lg:pt-16 pt-6 flex lg:flex-row flex-col justify-around lg:gap-9 gap-5">
+    <div className="border-t border-black border-opacity-20 lg:pt-16 pt-6 grid grid-cols-2  justify-around lg:gap-9 gap-5">
       {about.map((item) => (
         <div className="flex lg:gap-5 gap-2 flex-col" key={item.id}>
           <h1 className="font-bold lg:text-[24px] md:text-[18px] text-sm  ">
