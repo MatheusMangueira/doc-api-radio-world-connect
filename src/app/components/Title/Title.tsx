a105499b281b6a505e7ea36772ffd4da2610d561
MatheusMangueira <matheusmangueira16@hotmail.com>
2023-10-27 01:19:39 -0300

    fix: Color change

--- src/app/components/Title/Title.tsx
+++ src/app/components/Title/Title.tsx
@@ -6,7 +6,7 @@ type TitleProps = {
 export const Title = ({ name, span }: TitleProps) => {
   return (
     <>
-      <h1 className="lg:text-6xl md:text-5xl text-2xl text-[#ffffff] font-bold text-center drop-shadow-xl ">
+      <h1 className="lg:text-6xl md:text-5xl text-2xl text-[#808080] font-bold text-center drop-shadow-xl ">
         {name} <span className="text-[#3B06D0]">{span}</span>
       </h1>
     </>
