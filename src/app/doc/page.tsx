2c2e3f0c5f655fca757cf00c760e69fbb6c93608
MatheusMangueira <matheusmangueira16@hotmail.com>
2023-10-27 01:20:05 -0300

    feat: create page for Doc

---
+++ src/app/doc/page.tsx
@@ -0,0 +1,9 @@
+import { DocView } from '../views/docView';
+
+export default function Doc() {
+  return (
+    <div className="lg:px-10 p-2 max-w-[1920px] flex justify-center w-full">
+      <DocView />
+    </div>
+  );
+}
